0deee85a64f19afce6d0098265212fba7b2615f3
21f3002486 <21f3002486@ds.study.iitm.ac.in>
2025-02-05 07:05:32 +0530

    admin is able to search for profos to review them

--- frontend/ui/src/components/ProfessionalRequests.vue
+++ frontend/ui/src/components/ProfessionalRequests.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="d-flex justify-content-center">
-  <div v-if="profos.length !== 0">
+  <div class="container">
+    <div class="d-flex justify-content-between align-content-center mb-4">
+      <input type="text" v-model="professionalname" class="form-control m-2" placeholder="Search without typing anything to get all professionals">
+      <button class="btn btn-success m-2" @click="modifyprofessionals">Search</button>
+    </div>
+  <div v-if="buttonPressed">
+    <div v-if="filteredProfos.length !== 0">
+      <ExpandableCard 
+        v-for="professional in filteredProfos"
+        :key="professional.user_id"
+        :professional="professional"
+        :is_approved="professional.is_approved"
+        @approve="approveProfessional"
+      />
+    </div>
+    <div v-else>
+      There are no professionals with this name
+    </div>
+  </div>
+  <div v-else-if="profos.length !== 0">
     <div>
       <ExpandableCard 
         v-for="professional in profos"
@@ -24,10 +42,26 @@ import ExpandableCard from './ExpandableCard.vue';
     components: {ExpandableCard},
     data(){
       return{
-        profos: []
+        profos: [],
+        filteredProfos: [],
+        professionalname: '',
+        buttonPressed: false
       }
     },
     methods:{
+      modifyprofessionals(e){
+        e.preventDefault();
+        const inputPattern = this.professionalname.trim();
+        const regexPattern = inputPattern
+        .replace(/%/g, ".*")
+        .replace(/_/g, ".");
+      
+        const regex = new RegExp(`^.*${regexPattern}.*$`, "i");
+      
+        this.filteredProfos = this.profos.filter(p => regex.test(p.email_id));
+        console.log('this is being pressed')
+        this.buttonPressed = true;
+      },
       approveProfessional(user_id){
         console.log(user_id+" to be approved");
         // backend call here
